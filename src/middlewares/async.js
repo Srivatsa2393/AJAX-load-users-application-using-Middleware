3c734342a3acc054ddd4d287f61847cbbb374403
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-29 21:58:39 +0530

    handling unrelated actions

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,13 @@
 export default function({ dispatch }) {
   return next => action => {
-    console.log(action);
+    //console.log(action);
+    //if the action does not have a payload or the payload does not have a .then property
+    // we dont care about it, send it on
+    if(!action.payload || !action.payload.then){
+      return next(action);
+    }
 
-    next(action);
+    //next(action);
+    console.log('We dont have a promise', action);
   };
 }
